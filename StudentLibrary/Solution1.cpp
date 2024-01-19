6c756217064f402a5581bc3bb2c346cc2a7df6d9
unknown <fdubois@gamingcampus.local>
2024-01-19 13:48:24 +0100

    Exercice 1

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -2,10 +2,45 @@
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
+#include <iostream>
 #ifdef COMPILE_EXERCICE_1
+
+/* CONVERSION FORMULAS :
+*Celsius to Fahrenheit °F = 9 / 5 (°C)+32 //
+* Kelvin to Fahrenheit °F = 9 / 5 (K - 273) + 32 //
+* Fahrenheit to Celsius °C = 5 / 9 (°F - 32) //
+* Celsius to Kelvin K = °C + 273 //
+* Kelvin to Celsius °C = K - 273 //
+* Fahrenheit to Kelvin K = 5 / 9 (°F - 32) + 273 //
+* */
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
+	float _results;
+
+	//CELSIUS TO ->
+	if (_from == TemperatureUnits::CELSIUS, _to == TemperatureUnits::FAHRENHEIT) {
+		_results == 9 / 5 * (_value + 32);
+	}
+	if (_from == TemperatureUnits::CELSIUS, _to == TemperatureUnits::KELVIN) {
+		_results == _value + 273;
+	}
+
+	//FAHRENHEIT TO ->
+	if (_from == TemperatureUnits::FAHRENHEIT, _to == TemperatureUnits::CELSIUS) {
+		_results == 5 / 9 * (_value - 32);
+	}
+	if (_from == TemperatureUnits::FAHRENHEIT, _to == TemperatureUnits::KELVIN) {
+		_results == 5 / 9 * (_value - 32) + 273;
+	}
+
+	//KELVIN TO ->
+	if (_from == TemperatureUnits::KELVIN, _to == TemperatureUnits::CELSIUS) {
+		_results == _value - 273;
+	}
+	if (_from == TemperatureUnits::KELVIN, _to == TemperatureUnits::FAHRENHEIT) {
+		_results == 9 / 5 * (_value - 273) + 32;
+	}
 	return -1.0f;
 }
 
