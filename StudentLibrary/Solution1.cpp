8f29120b91ba84844579ccc6146bfcf19c9aab5f
unknown <fdubois@gamingcampus.local>
2024-01-19 14:23:21 +0100

    FF

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -16,16 +16,7 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	float _results;
-
-
-	//KELVIN TO ->
-	if (_from = TemperatureUnits::KELVIN, _to == TemperatureUnits::CELSIUS) {
-		_results == _value - 273;
-	}
-	if (_from = TemperatureUnits::KELVIN, _to == TemperatureUnits::FAHRENHEIT) {
-		_results == 9 / 5 * (_value - 273) + 32;
-	}
+	float _results = 0.0;
 
 	//CELSIUS TO ->
 	if (_from = TemperatureUnits::CELSIUS, _to == TemperatureUnits::FAHRENHEIT) {
@@ -33,6 +24,9 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
 	}
 	if (_from = TemperatureUnits::CELSIUS, _to == TemperatureUnits::KELVIN) {
 		_results == _value + 273;
+	}
+	if (_from = TemperatureUnits::CELSIUS, _to == TemperatureUnits::CELSIUS) {
+		_results == _value;
 	}
 
 	//FAHRENHEIT TO ->
@@ -42,7 +36,20 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
 	if (_from = TemperatureUnits::FAHRENHEIT, _to == TemperatureUnits::KELVIN) {
 		_results == 5 / 9 * (_value - 32) + 273;
 	}
+	if (_from = TemperatureUnits::FAHRENHEIT, _to == TemperatureUnits::FAHRENHEIT) {
+		_results == _value;
+	}
 
+	//KELVIN TO ->
+	if (_from = TemperatureUnits::KELVIN, _to == TemperatureUnits::CELSIUS) {
+		_results == _value - 273;
+	}
+	if (_from = TemperatureUnits::KELVIN, _to == TemperatureUnits::FAHRENHEIT) {
+		_results == 9 / 5 * (_value - 273) + 32;
+	}
+	if (_from = TemperatureUnits::KELVIN, _to == TemperatureUnits::KELVIN) {
+		_results == _value;
+	}
 	return -1.0f;
 }
 
